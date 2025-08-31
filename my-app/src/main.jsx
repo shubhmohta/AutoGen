5a3d7c4d3d8d2a496655da47af0e6c08457c6e4f
shubhmohta <shubhmohta@gmail.com>
2025-08-31 07:16:08 +0530

    Fix routing with HashRouter and add 404 handling

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Result from "./result";
 
-const basePath = import.meta.env.BASE_URL;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basePath}>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/result" element={<Result />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
