00e1877c0152eedb923bf459d4f1d9de8533a601
shubhmohta <shubhmohta@gmail.com>
2025-08-31 07:14:14 +0530

    Fix router base path

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,9 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Result from "./result";
 
+const basePath = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basePath}>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/result" element={<Result />} />
